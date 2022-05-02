e9fb3baf7453f11c73ebee37830b19b098864a41
jiyunnoh <ianjy1127@gmail.com>
2022-05-02 14:54:47 -0500

    Add input focus style

--- src/pages/analyzer.js
+++ src/pages/analyzer.js
@@ -1,11 +1,29 @@
 import { useContext } from "react";
 import UserContext from "../store/user-context";
+import styled from "styled-components";
+
+const Input = styled.input`
+    display: block;
+    margin: 0 auto;
+    text-align: center;
+    width: 70%;
+    height: 30px;
+    border: solid 1px #ccc;
+    border-radius: 20px;
+    margin-top: 100px;
+
+    &:focus {
+        outline: none;
+        box-shadow: 0 3px 10px rgb(0, 0, 0, 0.2);
+    }
+`;
+
 
 const Analyzer = () => {
     const userCtx = useContext(UserContext);
     return (
         <>
-            <h1>This is Analyzer page.</h1>
+            <Input type="text" placeholder="Enter Korean text to analyze" />
             <h2>{userCtx.name}</h2>
         </>
     );
